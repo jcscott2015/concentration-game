f93bf3a74e8c5876e0e384e63ea32721134cbbf9
John C. Scott <jcscott@scottcomm.com>
2017-12-19 11:09:51 -0800

    Initialize Project.
    Set up basic stack for the concentration game.

---
+++ webpack.config.dev.js
@@ -0,0 +1,59 @@
+import webpack from 'webpack';
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+export default {
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.json', 'map']
+  },
+  devtool: 'inline-source-map',
+  /* Defining path seems necessary for this to work consistently on Windows machines. */
+  entry: [path.resolve(__dirname, 'src/index.js')],
+  target: 'web',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), // Note: Physical files are only output by the production build task `npm run build`.
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: false,
+      debug: true,
+      noInfo: true // set to false to see a list of every file being bundled.
+    }),
+
+        // Create HTML file that includes reference to bundled JS.
+    new HtmlWebpackPlugin({ // Create HTML file that includes references to bundled CSS and JS.
+      template: 'src/index.html'
+    }),
+
+    new ExtractTextPlugin("index.css")
+  ],
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+        }, {
+      test: /(\.css|\.scss|\.sass)$/,
+      exclude: /node_modules/,
+      use: ExtractTextPlugin.extract({
+        fallback: "style-loader",
+        use: [{
+          loader: 'css-loader',
+          options: {
+            minimize: true,
+            sourceMap: true
+          }
+        }, {
+          loader: 'sass-loader',
+          options: {
+            includePaths: [path.resolve(__dirname, 'src', 'scss')],
+            sourceMap: true
+          }
+        }]
+      })
+    }]
+  }
+}
