d1f2b521147ed0a8d801cd0630e2744fee65c8ad
John C. Scott <jcscott@scottcomm.com>
2017-12-20 04:21:36 -0800

    Added Deck Fetch Functions

---
+++ src/model/get-cards.js
@@ -0,0 +1,33 @@
+import 'whatwg-fetch';
+
+/**
+ * Return promise of new shuffled deck of cards.
+ */
+export function getNewDeck() {
+  return get('new', 52);
+}
+
+/**
+ * Returns cards drawn from a new or existing deck using the deckofcardsapi.
+ * @param {str} deckId - 'new' or deck id number.
+ * @param {int} cardCount - number of cards
+ */
+function get(deckId, cardCount) {
+  return fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${cardCount}`).then(onSuccess, onError);
+}
+
+/**
+ * Returns json object on success of get fetch.
+ * @param {obj} response
+ */
+function onSuccess(response) {
+  return response.json();
+}
+
+/**
+ * Returns error of get fetch.
+ * @param {obj} error
+ */
+function onError(error) {
+  console.error(error); // eslint-disable-line no-console
+}
---
+++ src/model/get-cards.test.js
@@ -0,0 +1,13 @@
+/*eslint-disable no-unused-vars*/
+
+import { chai, should } from 'chai';
+import chaiFetchMock from 'chai-fetch-mock';
+import fetchMock from 'fetch-mock';
+import sinon from 'sinon';
+//import { getNewDeck } from './api/get-cards';
+should(); // Modifies `Object.prototype`
+
+describe('getNewDeck', () => {
+  it('should pass "new" and 52 as arguments to get()', () => {});
+  it('should get new deck of 52 cards', () => {});
+});
